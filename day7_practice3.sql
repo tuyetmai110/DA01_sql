dd8b655de4fcf6ecc613e611aa457b331208bccb
tuyetmai110 <nttm8cc@gmail.com>
2024-07-12 17:39:09 +0700

    Update day7_practice3.sql

--- day7_practice3.sql
+++ day7_practice3.sql
@@ -43,6 +43,9 @@ from Tweets
 where length(content)>15
 
 ---ex7
+select activity_date as day, count(distinct(user_id)) as active_users from Activity
+where activity_date between "2019-06-28" and "2019-07-27"
+group by activity_date;
 
 ---EX8 Number of Hires During Specific Time Period
 select count(id), extract(month from joining_date) AS MONTH
